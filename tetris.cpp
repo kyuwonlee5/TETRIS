51804e5fa29bd8bf81374912adc0a775265a0312
kyuwonlee5 <kyuwonlee5@gmail.com>
2023-10-12 23:03:57 +0900

    finally

--- tetris.cpp
+++ tetris.cpp
@@ -339,7 +339,7 @@ public:
         cout << "\t\t"; cout << "      @       @@@@@@@@@        @       @      @   @   @@@@@@@@@@@\n\n\n\n\n";
         cout << "\t\t"; cout << "                게임을 시작하려면 아무키나 누르세요.\n\n\n\n\n\n\n";
  
-        cout << "\t\t"; cout << "                   TetrisGame1.6 By SeokJinLee\n";
+        cout << "\t\t"; cout << "                   TetrisGame1.6 By QoneLee\n";
  
         getchar(); // 아무키 입력 기다림
         system("cls"); // 콘솔 창 clear
